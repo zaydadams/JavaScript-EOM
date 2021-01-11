ac41cb1e1bffac0b151aae0af87069bbf581be65
zay <zaydadams6@gmail.com>
2021-01-11 07:36:33 +0200

    update

--- main.js
+++ main.js
@@ -24,8 +24,23 @@ const app = ()=>{
 navSlide();
 
 
-//image
+const btn = document.querySelectorAll(".category button");
+const product = document.querySelectorAll(".itembox");
 
-function onClick(element) {
-    document.getElementById
-}
+btn.forEach(item => {
+    item.addEventListener("click", function (){
+        for(let i = 0; i < btn.length ; i++){
+            btn[i].classList.remove("active");
+        }          
+        item.classList.add("active");
+
+        product.forEach(show => {
+            show.style.display = "none";
+            let products=item.textContent.toLowerCase();
+            if(show.getAttribute("data-att")===
+            products || products ==="all"){
+                show.style.display = "block";
+            }
+        })
+    })
+})
